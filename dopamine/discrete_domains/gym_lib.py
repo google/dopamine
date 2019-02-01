202fa9e90aa61edabca92bc4b6f4f7895b95293b
Dopamine Team <no-reply@google.com>
2019-02-01 17:37:12 -0500

    Add agents using Fourier basis and linear Q function approximation
    PiperOrigin-RevId: 232039653

--- dopamine/discrete_domains/gym_lib.py
+++ dopamine/discrete_domains/gym_lib.py
@@ -25,6 +25,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import itertools
 import math
 
 
@@ -123,6 +124,98 @@ def cartpole_dqn_network(num_actions, network_type, state):
   return network_type(q_values)
 
 
+class FourierBasis(object):
+  """Fourier Basis linear function approximation.
+
+  Requires the ranges for each dimension, and is thus able to use only sine or
+  cosine (and uses cosine). So, this has half the coefficients that a full
+  Fourier approximation would use.
+
+  Many thanks to Will Dabney (wdabney@) for this implementation.
+
+  From the paper:
+  G.D. Konidaris, S. Osentoski and P.S. Thomas. (2011)
+  Value Function Approximation in Reinforcement Learning using the Fourier Basis
+  """
+
+  def __init__(self, nvars, min_vals=0, max_vals=None, order=3):
+    self.order = order
+    self.min_vals = min_vals
+    self.max_vals = max_vals
+    terms = itertools.product(range(order + 1), repeat=nvars)
+
+    # Removing first iterate because it corresponds to the constant bias
+    self.multipliers = tf.constant(
+        [list(map(int, x)) for x in terms][1:], dtype=tf.float32)
+
+  def scale(self, values):
+    shifted = values - self.min_vals
+    if self.max_vals is None:
+      return shifted
+
+    return shifted / (self.max_vals - self.min_vals)
+
+  def compute_features(self, features):
+    # Important to rescale features to be between [0,1]
+    scaled = self.scale(features)
+    return tf.cos(np.pi * tf.matmul(scaled, self.multipliers, transpose_b=True))
+
+
+@gin.configurable
+def fourier_dqn_network(min_vals,
+                        max_vals,
+                        num_actions,
+                        state,
+                        fourier_basis_order=3):
+  """Builds the function approximator used to compute the agent's Q-values.
+
+  It uses FourierBasis features and a linear layer.
+
+  Args:
+    min_vals: float, minimum attainable values (must be same shape as `state`).
+    max_vals: float, maximum attainable values (must be same shape as `state`).
+    num_actions: int, number of actions.
+    state: `tf.Tensor`, contains the agent's current state.
+    fourier_basis_order: int, order of the Fourier basis functions.
+
+  Returns:
+    The Q-values for DQN-style agents or logits for Rainbow-style agents.
+  """
+  net = tf.cast(state, tf.float32)
+  net = slim.flatten(net)
+
+  # Feed state through Fourier basis.
+  feature_generator = FourierBasis(
+      net.get_shape().as_list()[-1],
+      min_vals,
+      max_vals,
+      order=fourier_basis_order)
+  net = feature_generator.compute_features(net)
+
+  # Q-values are always linear w.r.t. last layer.
+  q_values = slim.fully_connected(
+      net, num_actions, activation_fn=None, biases_initializer=None)
+  return q_values
+
+
+def cartpole_fourier_dqn_network(num_actions, network_type, state):
+  """Builds the function approximator used to compute the agent's Q-values.
+
+  It uses the Fourier basis features and a linear function approximator.
+
+  Args:
+    num_actions: int, number of actions.
+    network_type: namedtuple, collection of expected values to return.
+    state: `tf.Tensor`, contains the agent's current state.
+
+  Returns:
+    net: _network_type object containing the tensors output by the network.
+  """
+  q_values = fourier_dqn_network(CARTPOLE_MIN_VALS, CARTPOLE_MAX_VALS,
+                                 num_actions, state)
+  return network_type(q_values)
+
+
 @gin.configurable
 def cartpole_rainbow_network(num_actions, num_atoms, support, network_type,
                              state):
@@ -163,6 +256,25 @@ def acrobot_dqn_network(num_actions, network_type, state):
   """
   q_values = _basic_discrete_domain_network(
       ACROBOT_MIN_VALS, ACROBOT_MAX_VALS, num_actions, state)
+  return network_type(q_values)
+
+
+@gin.configurable
+def acrobot_fourier_dqn_network(num_actions, network_type, state):
+  """Builds the function approximator used to compute the agent's Q-values.
+
+  It uses the Fourier basis features and a linear function approximator.
+
+  Args:
+    num_actions: int, number of actions.
+    network_type: namedtuple, collection of expected values to return.
+    state: `tf.Tensor`, contains the agent's current state.
+
+  Returns:
+    net: _network_type object containing the tensors output by the network.
+  """
+  q_values = fourier_dqn_network(ACROBOT_MIN_VALS, ACROBOT_MAX_VALS,
+                                 num_actions, state)
   return network_type(q_values)
 
 
