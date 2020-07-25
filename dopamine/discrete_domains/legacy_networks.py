f9aa819773e98e23aa8867a9f8c437d4aafb2b55
Alexandre Vassalotti <avassalotti@google.com>
2020-07-24 21:39:15 -0400

    Reimplement legacy TF-Slim networks using the Keras-based ones.
    PiperOrigin-RevId: 323111605

--- dopamine/discrete_domains/legacy_networks.py
+++ dopamine/discrete_domains/legacy_networks.py
@@ -14,14 +14,9 @@
 # limitations under the License.
 """Legacy (pre-Keras) network architectures."""
 
-import math
-
+from dopamine.discrete_domains import atari_lib
 from dopamine.discrete_domains import gym_lib
 import gin
-import numpy as np
-import tensorflow as tf
-from tensorflow.contrib import layers as contrib_layers
-from tensorflow.contrib import slim as contrib_slim
 
 
 ### ATARI networks ###
@@ -38,15 +33,9 @@ def nature_dqn_network(num_actions, network_type, state):
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  net = tf.cast(state, tf.float32)
-  net = net / 255
-  net = contrib_slim.conv2d(net, 32, [8, 8], stride=4)
-  net = contrib_slim.conv2d(net, 64, [4, 4], stride=2)
-  net = contrib_slim.conv2d(net, 64, [3, 3], stride=1)
-  net = contrib_slim.flatten(net)
-  net = contrib_slim.fully_connected(net, 512)
-  q_values = contrib_slim.fully_connected(net, num_actions, activation_fn=None)
-  return network_type(q_values)
+  model = atari_lib.NatureDQNNetwork(num_actions, network_type)
+  net = model(state)
+  return network_type(net.q_values)
 
 
 def rainbow_network(num_actions, num_atoms, support, network_type, state):
@@ -62,30 +51,9 @@ def rainbow_network(num_actions, num_atoms, support, network_type, state):
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  weights_initializer = contrib_slim.variance_scaling_initializer(
-      factor=1.0 / np.sqrt(3.0), mode='FAN_IN', uniform=True)
-
-  net = tf.cast(state, tf.float32)
-  net = net / 255
-  net = contrib_slim.conv2d(
-      net, 32, [8, 8], stride=4, weights_initializer=weights_initializer)
-  net = contrib_slim.conv2d(
-      net, 64, [4, 4], stride=2, weights_initializer=weights_initializer)
-  net = contrib_slim.conv2d(
-      net, 64, [3, 3], stride=1, weights_initializer=weights_initializer)
-  net = contrib_slim.flatten(net)
-  net = contrib_slim.fully_connected(
-      net, 512, weights_initializer=weights_initializer)
-  net = contrib_slim.fully_connected(
-      net,
-      num_actions * num_atoms,
-      activation_fn=None,
-      weights_initializer=weights_initializer)
-
-  logits = tf.reshape(net, [-1, num_actions, num_atoms])
-  probabilities = contrib_layers.softmax(logits)
-  q_values = tf.reduce_sum(support * probabilities, axis=2)
-  return network_type(q_values, logits, probabilities)
+  model = atari_lib.RainbowNetwork(num_actions, num_atoms, support)
+  net = model(state)
+  return network_type(net.q_values, net.logits, net.probabilities)
 
 
 def implicit_quantile_network(num_actions, quantile_embedding_dim,
@@ -102,45 +70,10 @@ def implicit_quantile_network(num_actions, quantile_embedding_dim,
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  weights_initializer = contrib_slim.variance_scaling_initializer(
-      factor=1.0 / np.sqrt(3.0), mode='FAN_IN', uniform=True)
-
-  state_net = tf.cast(state, tf.float32)
-  state_net = state_net / 255
-  state_net = contrib_slim.conv2d(
-      state_net, 32, [8, 8], stride=4, weights_initializer=weights_initializer)
-  state_net = contrib_slim.conv2d(
-      state_net, 64, [4, 4], stride=2, weights_initializer=weights_initializer)
-  state_net = contrib_slim.conv2d(
-      state_net, 64, [3, 3], stride=1, weights_initializer=weights_initializer)
-  state_net = contrib_slim.flatten(state_net)
-  state_net_size = state_net.get_shape().as_list()[-1]
-  state_net_tiled = tf.tile(state_net, [num_quantiles, 1])
-
-  batch_size = state_net.get_shape().as_list()[0]
-  quantiles_shape = [num_quantiles * batch_size, 1]
-  quantiles = tf.random.uniform(
-      quantiles_shape, minval=0, maxval=1, dtype=tf.float32)
-
-  quantile_net = tf.tile(quantiles, [1, quantile_embedding_dim])
-  pi = tf.constant(math.pi)
-  quantile_net = tf.cast(tf.range(
-      1, quantile_embedding_dim + 1, 1), tf.float32) * pi * quantile_net
-  quantile_net = tf.cos(quantile_net)
-  quantile_net = contrib_slim.fully_connected(
-      quantile_net, state_net_size, weights_initializer=weights_initializer)
-  # Hadamard product.
-  net = tf.multiply(state_net_tiled, quantile_net)
-
-  net = contrib_slim.fully_connected(
-      net, 512, weights_initializer=weights_initializer)
-  quantile_values = contrib_slim.fully_connected(
-      net,
-      num_actions,
-      activation_fn=None,
-      weights_initializer=weights_initializer)
-
-  return network_type(quantile_values=quantile_values, quantiles=quantiles)
+  model = atari_lib.ImplicitQuantileNetwork(num_actions, quantile_embedding_dim)
+  net = model(state, num_quantiles)
+  return network_type(quantile_values=net.quantile_values,
+                      quantiles=net.quantiles)
 
 
 ### Generic Gym networks ###
@@ -162,20 +95,9 @@ def _basic_discrete_domain_network(min_vals, max_vals, num_actions, state,
   Returns:
     The Q-values for DQN-style agents or logits for Rainbow-style agents.
   """
-  net = tf.cast(state, tf.float32)
-  net = contrib_slim.flatten(net)
-  net -= min_vals
-  net /= max_vals - min_vals
-  net = 2.0 * net - 1.0  # Rescale in range [-1, 1].
-  net = contrib_slim.fully_connected(net, 512)
-  net = contrib_slim.fully_connected(net, 512)
-  if num_atoms is None:
-    # We are constructing a DQN-style network.
-    return contrib_slim.fully_connected(net, num_actions, activation_fn=None)
-  else:
-    # We are constructing a Rainbow-style network.
-    return contrib_slim.fully_connected(
-        net, num_actions * num_atoms, activation_fn=None)
+  layer = gym_lib.BasicDiscreteDomainNetwork(min_vals, max_vals, num_actions,
+                                             num_atoms)
+  return layer(state)
 
 
 @gin.configurable
@@ -192,9 +114,9 @@ def cartpole_dqn_network(num_actions, network_type, state):
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  q_values = _basic_discrete_domain_network(
-      gym_lib.CARTPOLE_MIN_VALS, gym_lib.CARTPOLE_MAX_VALS, num_actions, state)
-  return network_type(q_values)
+  model = gym_lib.CartpoleDQNNetwork(num_actions)
+  net = model(state)
+  return network_type(net.q_values)
 
 
 @gin.configurable
@@ -217,21 +139,9 @@ def fourier_dqn_network(min_vals,
   Returns:
     The Q-values for DQN-style agents or logits for Rainbow-style agents.
   """
-  net = tf.cast(state, tf.float32)
-  net = contrib_slim.flatten(net)
-
-  # Feed state through Fourier basis.
-  feature_generator = gym_lib.FourierBasis(
-      net.get_shape().as_list()[-1],
-      min_vals,
-      max_vals,
-      order=fourier_basis_order)
-  net = feature_generator.compute_features(net)
-
-  # Q-values are always linear w.r.t. last layer.
-  q_values = contrib_slim.fully_connected(
-      net, num_actions, activation_fn=None, biases_initializer=None)
-  return q_values
+  model = gym_lib.FourierDQNNetwork(min_vals, max_vals, num_actions,
+                                    fourier_basis_order)
+  return model(state).q_values
 
 
 def cartpole_fourier_dqn_network(num_actions, network_type, state):
@@ -247,10 +157,9 @@ def cartpole_fourier_dqn_network(num_actions, network_type, state):
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  q_values = fourier_dqn_network(gym_lib.CARTPOLE_MIN_VALS,
-                                 gym_lib.CARTPOLE_MAX_VALS,
-                                 num_actions, state)
-  return network_type(q_values)
+  model = gym_lib.CartpoleFourierDQNNetwork(num_actions)
+  net = model(state)
+  return network_type(net.q_values)
 
 
 @gin.configurable
@@ -268,13 +177,9 @@ def cartpole_rainbow_network(num_actions, num_atoms, support, network_type,
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  net = _basic_discrete_domain_network(
-      gym_lib.CARTPOLE_MIN_VALS, gym_lib.CARTPOLE_MAX_VALS, num_actions, state,
-      num_atoms=num_atoms)
-  logits = tf.reshape(net, [-1, num_actions, num_atoms])
-  probabilities = contrib_layers.softmax(logits)
-  q_values = tf.reduce_sum(support * probabilities, axis=2)
-  return network_type(q_values, logits, probabilities)
+  model = gym_lib.CartpoleRainbowNetwork(num_actions, num_atoms, support)
+  net = model(state)
+  return network_type(net.q_values, net.logits, net.probabilities)
 
 
 @gin.configurable
@@ -291,9 +196,9 @@ def acrobot_dqn_network(num_actions, network_type, state):
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  q_values = _basic_discrete_domain_network(
-      gym_lib.ACROBOT_MIN_VALS, gym_lib.ACROBOT_MAX_VALS, num_actions, state)
-  return network_type(q_values)
+  model = gym_lib.AcrobotDQNNetwork(num_actions)
+  net = model(state)
+  return network_type(net.q_values)
 
 
 @gin.configurable
@@ -310,10 +215,9 @@ def acrobot_fourier_dqn_network(num_actions, network_type, state):
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  q_values = fourier_dqn_network(gym_lib.ACROBOT_MIN_VALS,
-                                 gym_lib.ACROBOT_MAX_VALS,
-                                 num_actions, state)
-  return network_type(q_values)
+  model = gym_lib.AcrobotFourierDQNNetwork(num_actions)
+  net = model(state)
+  return network_type(net.q_values)
 
 
 @gin.configurable
@@ -331,10 +235,6 @@ def acrobot_rainbow_network(num_actions, num_atoms, support, network_type,
   Returns:
     net: _network_type object containing the tensors output by the network.
   """
-  net = _basic_discrete_domain_network(
-      gym_lib.ACROBOT_MIN_VALS, gym_lib.ACROBOT_MAX_VALS, num_actions, state,
-      num_atoms=num_atoms)
-  logits = tf.reshape(net, [-1, num_actions, num_atoms])
-  probabilities = contrib_layers.softmax(logits)
-  q_values = tf.reduce_sum(support * probabilities, axis=2)
-  return network_type(q_values, logits, probabilities)
+  model = gym_lib.AcrobotRainbowNetwork(num_actions, num_atoms, support)
+  net = model(state)
+  return network_type(net.q_values, net.logits, net.probabilities)
