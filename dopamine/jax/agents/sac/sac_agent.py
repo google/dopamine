aadbf93350bd96808595cb3779fcf31d45453fad
Dopamine Team <no-reply@google.com>
2022-07-15 17:57:55 -0400

    Use jax.tree_util.tree_map in place of deprecated tree_multimap.
    The latter is a simple alias of the former, so this change is a no-op.
    
    PiperOrigin-RevId: 461246763

--- dopamine/jax/agents/sac/sac_agent.py
+++ dopamine/jax/agents/sac/sac_agent.py
@@ -432,8 +432,8 @@ class SACAgent(dqn_agent.JaxDQNAgent):
       return (self.target_smoothing_coefficient * online_p +
               (1 - self.target_smoothing_coefficient) * target_p)
 
-    self.target_params = jax.tree_multimap(_sync_weights, self.target_params,
-                                           self.network_params)
+    self.target_params = jax.tree_map(_sync_weights, self.target_params,
+                                      self.network_params)
 
   def begin_episode(self, observation):
     """Returns the agent's first action for this episode.
