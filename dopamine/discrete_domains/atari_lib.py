78a8670ed2a62bd4588ca33c8fd2ffee7b88d8d8
Mattia Colbertaldo <100996597+Mattia-Colbertaldo@users.noreply.github.com>
2023-11-08 14:49:50 +0100

    Update atari_lib.py

--- dopamine/discrete_domains/atari_lib.py
+++ dopamine/discrete_domains/atari_lib.py
@@ -160,8 +160,8 @@ class NatureDQNNetwork(tf.keras.Model):
 
   # Modification
   def reset_last_layer(self):
-        """Reset the last layer of the network."""
-        self.dense2 = tf.keras.layers.Dense(self.num_actions, name='fully_connected')
+    """Reset the last layer of the network."""
+    self.dense2 = tf.keras.layers.Dense(self.num_actions, name='fully_connected')
 
   def call(self, state):
     """Creates the output tensor/op given the state tensor as input.
